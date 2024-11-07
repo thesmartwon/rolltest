8c845d6ec1d671c7cd4958dc2dbd06d59fd1ddf0
thesmartwon <thesmartwon@pm.me>
2024-11-07 16:23:52 -0500

    reproduction of changing chunks

---
+++ main.js
@@ -0,0 +1,5 @@
+import './main.css';
+import { mount } from "svelte";
+import init from './lib.js';
+
+console.log(init(), mount);
---
+++ watch.js
@@ -0,0 +1,4 @@
+import { watch } from 'rolldown';
+import config from './rolldown.config.js';
+
+await watch(config);
