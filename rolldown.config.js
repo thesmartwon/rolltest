5ce8706c5c3619dbda387900d776611ab3b7fedc
thesmartwon <thesmartwon@pm.me>
2024-11-07 16:54:33 -0500

    even simpler

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,28 +1,10 @@
 import { defineConfig } from 'rolldown'
 
-let files = new Set();
-
 export default defineConfig({
 	input: {
 		entry: 'main.js',
 	},
-	plugins: [{
-		name: "check for bundle change",
-		writeBundle(_, b) {
-			const fileList = Object.keys(b);
-			const newFiles = new Set(fileList);
-			if (files.size) {
-				const added = Array.from(newFiles.difference(files));
-				const removed = Array.from(files.difference(newFiles));
-				if (added.length || removed.length) {
-					console.error({ removed, added });
-				}
-			}
-			files = newFiles;
-		},
-	}],
 	output: {
-		entryFileNames: "[name]-[hash].js",
 		advancedChunks: {
 			groups: [
 				{ name: "vendor", test: /node_modules/ },
--- watch.js
+++
@@ -1,4 +0,0 @@
-import { watch } from 'rolldown';
-import config from './rolldown.config.js';
-
-await watch(config);
